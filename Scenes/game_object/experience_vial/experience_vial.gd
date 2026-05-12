84a73b8af3d4ba748b19db7778c4d2a74ac61bc1
azaratow <azaratow@gmail.com>
2026-05-12 19:48:13 +1000

    created a game event singleton and implemented an experience gain signal from exp vial to game event

--- Scenes/game_object/experience_vial/experience_vial.gd
+++ Scenes/game_object/experience_vial/experience_vial.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
-
+@export var experience_granted: float = 1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Area2D.area_entered.connect(on_area_entered)
 
 
 func on_area_entered(other_area: Area2D):
+	GameEvents.emit_experience_vial_collected(experience_granted)
 	queue_free()
